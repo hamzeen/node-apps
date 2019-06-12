108eb7a42e3ad85dd34277b598e0588d4b05884f
Hamzeen <hamzeen@gmail.com>
2019-06-13 00:10:28 +0200

    react app

---
+++ git-app/src/actions/utils.js
@@ -0,0 +1,32 @@
+const UNKNOWN_LANG = 'Unknown';
+const DEFAULT_DESC = 'No description';
+
+export function formatGistList(respArr, username) {
+  return respArr.map(gist => {
+    console.log("GIST::", gist);
+    const { files, name, id, owner } = gist;
+
+    const fileTypes = [];
+
+    return {
+      id,
+      username,
+      owner,
+      name: name || DEFAULT_DESC,
+      fileTypes,
+      forks: [],
+      loading: true
+    }
+  });
+}
+
+export function formatForkList(respArr, id) {
+  const forks = respArr.map(fork => {
+    const { avatar_url, login } = fork.owner;
+    return { avatar_url, name: login }
+  });
+
+  return { forks: forks || [], id };
+}
+
+const shortName = name => (name.length > 2) ? name.substring(0, 2).toUpperCase() : name || '-'
---
+++ git-app/src/components/GistView.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import SearchContainer from '../containers/SearchContainer';
+import ResultCard from './ResultCard';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: `${theme.spacing.unit * 3}px`
+  },
+  loader: {
+    marginLeft: '50%',
+    marginTop: '120px'
+  },
+  resultDivMove: {
+    margin: '0 auto',
+    marginTop: '20px'
+  },
+  resultDiv: {
+    transition: 'margin 700ms',
+    margin: '0 auto',
+    marginTop: '150px'
+  },
+  animater: {
+    marginTop: '20px'
+  },
+  animaterTop: {
+    marginTop: '20px'
+  },
+  defaultText: {
+    color: '#555555',
+    width: '100%',
+    textAlign: 'center',
+    marginTop: '70px',
+  },
+});
+
+const GistView = (props) => {
+  const { classes, gists, loading, fetchForks } = props;
+
+  const gistsResultCards = gists.map((gist) => {
+    console.log("GIST:::", gist);
+
+    return <ResultCard
+      key={gist.id}
+      id={gist.id}
+      gistname={gist.name}
+      owner={gist.owner}
+      username={gist.username}
+      fileTypes={gist.fileTypes}
+      fetchForks={fetchForks}
+      forks={gist.forks}
+      loading={gist.loading} />;
+  }, this)
+
+  return (
+    <div className={classes.container}>
+      <div style={{ gridColumnEnd: 'span 2' }}></div>
+      <div
+        className={!loading && gists.length === 0 ? classes.resultDiv : classes.resultDivMove}
+        style={{ gridColumnEnd: 'span 8' }}>
+        <SearchContainer />
+        {loading ? <CircularProgress className={classes.loader} /> :
+          gists.length > 0
+            ? <div style={{ width: '550px' }}> {gistsResultCards}</div>
+            : <div className={classes.defaultText}>
+              {'Please type a username and click search'}</div>
+        }
+      </div>
+      <div style={{ gridColumnEnd: 'span 2' }}></div>
+    </div>
+  )
+}
+
+GistView.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  message: PropTypes.string.isRequired
+}
+
+export default withStyles(styles)(GistView);
